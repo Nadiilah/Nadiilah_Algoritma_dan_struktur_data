60aadae56dce6eac4e62b6e2324e9e4826d9d4e1
Nadiilah <nadiilahdila067@gmail.com>
2024-10-11 09:52:38 +0700

    Add files via upload

---
+++ python_operator keanggotaan.py
@@ -0,0 +1,24 @@
+perusahaan = 'Microsoft'
+list_pulau = ['Jawa', 'Sumatera', 'Sulawesi']
+# ini adalah dictionary, insyallah akan kita pelajari
+# di pertemuan-pertemuan yang akan datang
+mahasiswa = {
+    'nama': 'Lendis Fabri',
+    'asal': 'Lamongan'
+}
+print(
+    "Apakah 'c' ada di variable perusahaan?",
+    'c' in perusahaan 
+)
+print(
+    "Apakah 'z' tidak ada variable perusahaan?",
+    'c' not in perusahaan 
+)
+print(
+    "Apakah 'Madura' tidak ada di variable list_pulau?",
+"Madura" not in perusahaan
+)
+print(
+    "Apakah atribut 'nama' ada di variable mahasiswa?",
+    'nama' in mahasiswa
+)
