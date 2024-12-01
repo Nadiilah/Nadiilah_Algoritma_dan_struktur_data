b0afbcca803991fd38676cf1b57c3a5f6640fdcf
Nadiilah <nadiilahdila067@gmail.com>
2024-12-01 07:42:37 +0700

    Add files via upload

---
+++ phyton_nilai.py
@@ -0,0 +1,11 @@
+nilai = (int(input('masukkan nilai anda :')))
+if nilai >= 90:
+    print('nilai anda adalah A')
+elif nilai >= 80:
+    print('nilai anda adalah B')
+elif nilai >= 60:
+    print('nilai anda adalah C')
+elif nilai >= 40:
+    print('nilai anda adalah C')
+else :
+    print('nilai anda adalah D')
