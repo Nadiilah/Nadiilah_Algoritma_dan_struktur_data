21e31384f9727cac97375afddd401807458b7f33
Nadiilah <nadiilahdila067@gmail.com>
2024-12-01 07:37:48 +0700

    Add files via upload

---
+++ phyton_true.py
@@ -0,0 +1,5 @@
+if True :
+    print('kode program ini akan dieksekusi')
+if False :
+    print('kode ini tidak akan dieksekusi')
+print('kode program ini akan selalu dieksekusi karena tidak termasuk percabangan')
