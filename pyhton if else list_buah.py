ab5907e38e2f7f0694b8f113b468dcbc185eadfc
Nadiilah <nadiilahdila067@gmail.com>
2024-12-01 07:38:10 +0700

    Add files via upload

---
+++ pyhton if else list_buah.py
@@ -0,0 +1,7 @@
+list_buah = ['jeruk', 'mangga', 'semangka', 'naga']
+list_buah_dicari =input('masukkan nama buah dalam huruf kecil: ')
+if (list_buah_dicari in list_buah):
+    print('buah ditemukan dalam list')
+else :
+    print('list buah tidak ditemukan dalam list')
+    
