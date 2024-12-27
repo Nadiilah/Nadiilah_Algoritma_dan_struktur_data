1b6b74aee554ed813f435c16445cbd3ad8e5a100
Nadiilah <nadiilahdila067@gmail.com>
2024-12-27 10:43:16 +0700

    Add files via upload

---
+++ python input usia.py
@@ -0,0 +1,11 @@
+usia = (int(input('masukkan usia anda :')))
+if usia <= 5:
+    print('usia anda adalah Balita')
+elif usia <= 12:
+    print('usia anda adalah Anak-anak')
+elif usia <= 17:
+    print('usia anda adalah Remaja')
+elif usia <= 59:
+    print('usia anda adalah Dewasa')
+else :
+    print('usia anda adalah Lansia')
